e08cf9e186994e14abe2c091a455808de9b5861e
3-ark <11862956+3-ark@users.noreply.github.com>
2025-04-20 15:02:10 +0800

    7.1 update

--- src/sidePanel/Docs.tsx
+++ src/sidePanel/Docs.tsx
@@ -22,7 +22,7 @@ export const Docs = () => (
     onClick={() => window.open("https://github.com/3-ark/Bruside/blob/master/DOCS.md", "_blank")}
   >
     <Flex alignItems="center" justifyContent="space-between">
-      <Text>V7.0</Text>
+      <Text>V7.1</Text>
     </Flex>
   </Box>
 );
