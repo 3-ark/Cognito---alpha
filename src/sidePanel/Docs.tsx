7db65c8340534816d84f506a4ad257917d11b5cf
3-ark <11862956+3-ark@users.noreply.github.com>
2025-04-22 11:56:40 +0800

    0.8.2 cleanup redundant logic checking for background activities

--- src/sidePanel/Docs.tsx
+++ src/sidePanel/Docs.tsx
@@ -22,7 +22,7 @@ export const Docs = () => (
     onClick={() => window.open("https://github.com/3-ark/Cognito/blob/master/DOCS.md", "_blank")}
   >
     <Flex alignItems="center" justifyContent="space-between">
-      <Text>0.8.1</Text>
+      <Text>0.8.2</Text>
     </Flex>
   </Box>
 );
